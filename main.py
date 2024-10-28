1c3ec47620893ff371a8f998edfa178c99f1870e
Samanta Wiese <samantaws@gmail.com>
2024-10-28 11:51:09 -0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,51 @@
+import pyautogui
+import time
+# Passo a passo
+# Passo 1: Entrar no sistema
+pyautogui.PAUSE = 0.3
+
+pyautogui.press("win")
+pyautogui.write("opera")
+pyautogui.press("enter")
+pyautogui.write("https://dlp.hashtagtreinamentos.com/python/intensivao/login")
+pyautogui.press("enter")
+time.sleep(3)
+
+# Passo 2: Fazer login
+pyautogui.click(x=685, y=451)
+pyautogui.write("xunda@gmail.com")
+pyautogui.press("tab") # passando pro próximo campo
+pyautogui.write("sua senha")
+pyautogui.click(x=955, y=638) # clique no botao de login
+time.sleep(3)
+
+# Passo 3: Importar a base de produtos pra cadastrar
+import pandas as pd
+
+tabela = pd.read_csv("produtos.csv")
+
+print(tabela)
+
+# Passo 4: Cadastrar um produto
+for linha in tabela.index:
+    pyautogui.click(x=653, y=294)
+    codigo = tabela.loc[linha, "codigo"]
+    pyautogui.write(str(codigo))
+    pyautogui.press("tab")
+    pyautogui.write(str(tabela.loc[linha, "marca"]))
+    pyautogui.press("tab")
+    pyautogui.write(str(tabela.loc[linha, "tipo"]))
+    pyautogui.press("tab")
+    pyautogui.write(str(tabela.loc[linha, "categoria"]))
+    pyautogui.press("tab")
+    pyautogui.write(str(tabela.loc[linha, "preco_unitario"]))
+    pyautogui.press("tab")
+    pyautogui.write(str(tabela.loc[linha, "custo"]))
+    pyautogui.press("tab")
+    obs = tabela.loc[linha, "obs"]
+    if not pd.isna(obs):
+        pyautogui.write(str(tabela.loc[linha, "obs"]))
+    pyautogui.press("tab")
+    pyautogui.press("enter")
+    pyautogui.scroll(5000)
+
